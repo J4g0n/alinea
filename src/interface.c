9b9a1d46bdbb9009d2692f510c44ad1a9d940422
J4g0n <simon.andreux@gmail.com>
2012-12-26 19:22:52 +0100

    interface complète, reste dossier

--- src/interface.c
+++ src/interface.c
@@ -241,8 +241,223 @@ int menuInversion()
 
 int menuOperation()
 {
-	int i;
-	return i;
+	Matrix m,m1,result;
+	int action,act,i,j,row,column,row1,column1;
+	int dim=0;
+	action=-1;
+	while(act!=0)
+	{
+		for(i=0;i<40;i++)
+			printf("-");
+		printf("\n\nOPERATIONS: \n");
+		for(i=0;i<4;i++)
+		{
+			printf("\t%d./ ",i);
+			switch(i)
+			{
+				case 0:
+					printf("Retourner au menu ;\n");
+					break;
+				case 1:
+					printf("Echelonner une matrice par le pivot de gauss ;\n");
+					break;
+				case 2:
+					printf("Additionner deux matrices ;\n");
+					break;
+				case 3:
+					printf("Multiplication de deux matrices.\n");
+					break;
+				default:
+					break;
+			}
+		}
+		printf("\n\n");
+		for(i=0;i<40;i++)
+			printf("-");
+		printf("\n\n");
+
+		printf("Que voulez-vous faire?\n");
+		scanf("%d",&act);
+		printf("\n\n");
+
+		switch(act)
+		{
+			case 1:
+				printf("Donnez la dimension de la matrice.\n");
+				scanf("%d",&dim);
+				printf("\n");
+
+				printf("Souhaitez-vous remplir la matrice ou la créer	aléatoirement?\n");
+				printf("\t1./ Je remplis la matrice moi-même\n");
+				printf("\t2./ Je fais confiance au hasard\n");
+				scanf("%d",&action);
+				printf("\n\n");
+
+				if((dim>0)&&(action>0)&&(action<3))
+				{
+					m=newMatrix(dim,dim);
+
+					if(action==1)
+					{
+						for(i=0;i<dim;i++)
+						{
+							for(j=0;j<dim;j++)
+							{
+								printf("Donnez le %dème coefficient pour la ligne %d:	",j,i);
+								scanf("%f",&m->mat[i*dim+j]);
+							}
+						}
+					}
+					if(action==2)	
+					{
+						fillMatrix(m);
+					}
+
+					printf("\nVoici la matrice: \n");
+					afficheMatrice(m);
+					printf("\n");
+
+					result=gauss(m);
+
+					printf("On obtient: \n");
+					afficheMatrice(result);
+
+					deleteMatrix(result);
+				}	
+				break;
+			case 2: 
+				printf("Donnez les dimensions de la matrice.\n");
+				printf("\tNombre de lignes: ");
+				scanf("%d",&row);
+				printf("\tNombre de colonnes: ");
+				scanf("%d",&column);
+				printf("\n");
+
+				printf("Souhaitez-vous remplir les matrices ou les créer aléatoirement?\n");
+				printf("\t1./ Je remplis les matrices moi-même\n");
+				printf("\t2./ Je fais confiance au hasard\n");
+				scanf("%d",&action);
+				printf("\n\n");
+
+				if((row>0)&&(column>0))
+				{
+					m=newMatrix(row,column);
+					m1=newMatrix(row,column);
+
+					if(action==1)
+					{
+						printf("Matrice 1: \n");
+						for(i=0;i<row;i++)
+						{
+							for(j=0;j<column;j++)
+							{
+								printf("%dème ligne, %d colonne:	",i,j);
+								scanf("%f",&m->mat[i*column+j]);
+							}
+						}
+						printf("\n\nMatrice 2: \n");
+						for(i=0;i<row;i++)
+						{
+							for(j=0;j<column;j++)
+							{
+								printf("%dème ligne, %d colonne:	",i,j);
+								scanf("%f",&m1->mat[i*column+j]);
+							}
+						}
+					}
+					if(action==2)	
+					{
+						fillMatrix(m);
+						fillMatrix(m1);
+					}
+
+					printf("\nVoici les matrices: \n");
+					afficheMatrice(m);
+					afficheMatrice(m1);
+					printf("\n");
+
+					result=addition(m,m1);
+
+					printf("On obtient: \n");
+					afficheMatrice(result);
+
+					deleteMatrix(result);
+					deleteMatrix(m);
+					deleteMatrix(m1);
+				}			
+				break;
+			case 3:
+				printf("Donnez les dimensions des matrices.\n");
+				printf("Matrice 1:\n");
+				printf("\tNombre de lignes: ");
+				scanf("%d",&row);
+				printf("\tNombre de colonnes: ");
+				scanf("%d",&column);
+				printf("Matrice 2:\n");
+				printf("\tNombre de lignes: ");
+				scanf("%d",&row1);
+				printf("\tNombre de colonnes: ");
+				scanf("%d",&column1);
+				printf("\n");
+
+				if((column==row1)&&(row>0)&&(column>0)&&(column1>0))
+				{
+					printf("Souhaitez-vous remplir les matrices ou les créer aléatoirement?\n");
+					printf("\t1./ Je remplis les matrices moi-même\n");
+					printf("\t2./ Je fais confiance au hasard\n");
+					scanf("%d",&action);
+					printf("\n\n");
+
+					m=newMatrix(row,column);
+					m1=newMatrix(row1,column1);
+
+					if(action==1)
+					{
+						printf("Matrice 1: \n");
+						for(i=0;i<row;i++)
+						{
+							for(j=0;j<column;j++)
+							{
+								printf("%dème ligne, %dème colonne:	",i,j);
+								scanf("%f",&m->mat[i*column+j]);
+							}
+						}
+						printf("Matrice 2: \n");
+						for(i=0;i<row1;i++)
+						{
+							for(j=0;j<column1;j++)
+							{
+								printf("%dème ligne, %dème colonne:	",i,j);
+								scanf("%f",&m1->mat[i*column1+j]);
+							}
+						}
+					}
+					if(action==2)	
+					{
+						fillMatrix(m);
+						fillMatrix(m1);
+					}
+
+					printf("\nVoici les matrices: \n");
+					afficheMatrice(m);
+					afficheMatrice(m1);
+					printf("\n");
+
+					result=multiplication(m,m1);
+
+					printf("On obtient: \n");
+					afficheMatrice(result);
+
+					deleteMatrix(result);
+					deleteMatrix(m);
+					deleteMatrix(m1);
+				}
+				break;
+			default:
+				break;
+		}
+	}
+	return action;
 }
 
 int menuDecomposition()
